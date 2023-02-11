3d3dd93d1943ba2b7771e8833ffb22a8b8815eb9
dudadiass <diasnights@gmail.com>
2023-02-11 20:55:56 -0300

    algorithm expiry date validation

--- bank/lib/validations/card_validations/expiry_date_validation.dart
+++ bank/lib/validations/card_validations/expiry_date_validation.dart
@@ -1,10 +1,32 @@
 void main() {
-  String expirydate = "12/2028";
-  final exp = RegExp(r"^(([0-9]{1,2})(\/)([0-9]{4}))$");
+  String expirydate = "2/2024";
+  final exp = RegExp(r"^(([1-9]{1,2})(\/)([0-9]{4}))$");
 
-  if (exp.hasMatch(expirydate)) {
+  if (exp.hasMatch(expirydate) && dateIsValid(expirydate)) {
     print("the card is valid");
   } else {
     print("the card is invalid");
+  }
+}
+
+List<int> dateSplitted(String date) {
+  return date.split('/').map((e) => int.parse(e)).toList();
+}
+
+bool dateIsValid(String date) {
+  dynamic month = dateSplitted(date);
+  int monthProvided = month[0];
+
+  dynamic year = dateSplitted(date);
+  int yearProvided = year[1];
+  int actualYear = DateTime.now().year;
+  int expiryYear = actualYear + 6;
+
+  if (yearProvided > actualYear &&
+      yearProvided <= expiryYear &&
+      monthProvided <= 12) {
+    return true;
+  } else {
+    return false;
   }
 }
