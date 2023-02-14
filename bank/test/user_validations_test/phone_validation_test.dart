60637b0981382ac70e08381f9a1ff15f34412e60
dudadiass <diasnights@gmail.com>
2023-02-14 09:14:36 -0300

    phone validation test update

--- bank/test/user_validations_test/phone_validation_test.dart
+++ bank/test/user_validations_test/phone_validation_test.dart
@@ -2,7 +2,9 @@ import 'package:bank/validations/user_validations/phone_validation.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('should return String if the phone has not a DDD neither number 9', () {
+  test(
+      'should return String if the phone has not a DDD neither started with number 9',
+      () {
     final phoneExpected = phoneValidation('97826946');
     expect(phoneExpected, isNotNull);
   });
@@ -11,17 +13,20 @@ void main() {
     expect(phoneExpected, isNotNull);
   });
 
-  test('should return String if the phone has not a number 9', () {
+  test(
+      'should return String if the phone has DDD but has not started with number 9',
+      () {
     final phoneExpected = phoneValidation('4197826946');
     expect(phoneExpected, isNotNull);
   });
-  test('should return null if the phone has DDD and number 9', () {
+  test('should return null if the phone has DDD and started with number 9', () {
     final phoneExpected = phoneValidation('41997826946');
     expect(phoneExpected, null);
   });
-  test('should return null if the phone has space betwen DDD others numbers',
+  test(
+      'should return null if the phone has space betwen DDD and others numbers',
       () {
-    final phoneExpected = phoneValidation('41 123451234');
+    final phoneExpected = phoneValidation('41 997826946');
     expect(phoneExpected, null);
   });
   test(
@@ -31,7 +36,7 @@ void main() {
     expect(phoneExpected, null);
   });
   test(
-      'should return null if the DDD phone has parentheses and contains space btween DDD and others numbers',
+      'should return null if the DDD phone has parentheses and has space btween DDD and others numbers',
       () {
     final phoneExpected = phoneValidation('(41) 99782-6946');
     expect(phoneExpected, null);
